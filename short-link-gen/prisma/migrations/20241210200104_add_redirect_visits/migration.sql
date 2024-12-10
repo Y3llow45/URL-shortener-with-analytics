fa1517b469fe1b52d2adf6cca0c39701093928ab
Борислав Христов <hristovborislav45@gmail.com>
2024-12-10 22:06:44 +0200

    Update db

---
+++ short-link-gen/prisma/migrations/20241210200104_add_redirect_visits/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - The primary key for the `Link` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The `id` column on the `Link` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Link" DROP CONSTRAINT "Link_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ADD CONSTRAINT "Link_pkey" PRIMARY KEY ("id");
