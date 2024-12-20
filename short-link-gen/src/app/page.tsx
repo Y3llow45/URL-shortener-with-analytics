ec274d87bfd2638aff3c2a53b6641c892562a94c
Борислав Христов <hristovborislav45@gmail.com>
2024-12-20 16:38:17 +0200

    Add svgs and update home page

--- short-link-gen/src/app/page.tsx
+++ short-link-gen/src/app/page.tsx
@@ -4,25 +4,24 @@ import './homePage.css';
 export default function Home() {
   return (
     <div className="container">
-      <h1>Welcome to URL Shortener with Analytics</h1>
-      <p>
+      <h1 className="welcome-text">Welcome to URL Shortener with Analytics</h1>
+      <p className="description">
         This website allows you to shorten long URLs, generate QR codes for them, and track the number of visitors. 
         You can also set an expiration date for your shortened URLs.
       </p>
       <h2>Features</h2>
-      <ul>
+      <ul className="features-list">
         <li>Shorten long URLs</li>
         <li>Generate QR codes for shortened URLs</li>
         <li>Track the number of visitors</li>
         <li>Set expiration dates for URLs</li>
       </ul>
       <h2>Tech Stack</h2>
-      <ul>
-        <li>React</li>
-        <li>Next.js</li>
-        <li>Redux Toolkit</li>
-        <li>Prisma</li>
-        <li>TypeScript</li>
+      <ul className="tech-stack">
+        <li><img className='techimg' src="next.svg" alt="Next.js" /> Next.js</li>
+        <li><img className='techimg' src="redux.svg" alt="Redux Toolkit" /> Redux Toolkit</li>
+        <li><img className='techimg' src="prisma.svg" alt="Prisma" /> Prisma</li>
+        <li><img className='techimg' src="typescript.svg" alt="TypeScript" /> TypeScript</li>
       </ul>
     </div>
   );
