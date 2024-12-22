522c132891f179efb43b18dfdf90bd9f92f23e7c
Борислав Христов <hristovborislav45@gmail.com>
2024-12-22 13:16:52 +0200

    Update route.ts

--- short-link-gen/src/app/[slug]/route.ts
+++ short-link-gen/src/app/[slug]/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function GET(req: Request, {params}: { params: { slug: string } }) {
+export async function GET(req: NextRequest, {params}: { params: { slug: string } }) {
   const slug = params.slug; 
 
   if (!slug) {
