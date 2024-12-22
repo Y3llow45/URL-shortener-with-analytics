241287aa0b1d5b1c80262e25cda7165c75b382ec
Борислав Христов <hristovborislav45@gmail.com>
2024-12-22 13:38:13 +0200

    Update route.ts

--- short-link-gen/src/app/[slug]/route.ts
+++ short-link-gen/src/app/[slug]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse, NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function GET(req: NextRequest, context: { params: Promise<{ slug: string }> }) {
-  const {slug} = await context.params; 
+export async function GET(req: NextRequest, context: { params: { slug: string } }) {
+  const {slug} = context.params; 
 
   if (!slug) {
     return new Response('Slug not provided', { status: 400 });
