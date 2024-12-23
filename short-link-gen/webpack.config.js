590cc96e4a06699b17c47c6be41b7faf98d60d4d
Борислав Христов <hristovborislav45@gmail.com>
2024-12-23 18:39:49 +0200

    Update tests

---
+++ short-link-gen/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js', // Adjust the entry point as needed
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production', // Set the mode to 'production' or 'development'
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
